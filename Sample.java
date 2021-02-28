fb7a02c45f1920b9a56de5bf39fed9a8b0ee3c8f
Ravindra Desai <pravm@allstate.com>
2021-02-28 19:27:59 +0530

    Created New file

---
+++ Sample.java
@@ -0,0 +1,10 @@
+class public Hello
+{
+    public static void main(String[] args)
+    {
+        for(int i=0;i<=5;i++)
+        {
+            System.out.print("Wel-Come to ALLSTATE");
+        }
+    }
+}
